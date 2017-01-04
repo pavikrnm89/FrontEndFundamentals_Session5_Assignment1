34b8fda15559e19e3f8367702ff99356dd9dfdf9
pavikrnm89 <pavankarnam89@gmail.com>
2017-01-04 12:11:24 +0530

    Create app.js

---
+++ resources/app.js
@@ -0,0 +1,9 @@
+var name="Pavan Karnam";
+var age=27;
+var dateOfBirth=new Date("October 07, 1989")
+var placeOfBirth="Karatagi";
+
+console.log("Name="+name);
+console.log("Age="+age);
+console.log("Date of Birth="+dateOfBirth);
+console.log("Place of Birth="+placeOfBirth);
